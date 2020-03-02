ae33f10d01df830c75e6e5401c69088a44afb547
NITINCHOUREY <NITINCHOUREY.SEP@GMAIL.COM>
2020-03-03 01:03:06 +0530

    initial changes

--- automation/implementation/test.js
+++ automation/implementation/test.js
@@ -1,6 +1,7 @@
-
+const {I}=inject();
 
 
 Given('print something here',async function(){
+I.amOnPage("https://www.npmjs.com/");
     console.log("+++++++++++++++++++Hi nitin i am printing this+++++++++++++++");
 });
