9de48635c939158537bcfc2b8c0f4c4ad516e77e
NITINCHOUREY <NITINCHOUREY.SEP@GMAIL.COM>
2020-03-04 00:32:16 +0530

    initial changes

--- automation/implementation/test.js
+++ automation/implementation/test.js
@@ -5,4 +5,5 @@ Given('print something here',async function(){
 I.amOnPage("https://www.npmjs.com/");
 I.saveScreenshot("npmPage.png");
     console.log("+++++++++++++++++++Hi nitin i am printing this+++++++++++++++");
+    I.see("heysdf")
 });
